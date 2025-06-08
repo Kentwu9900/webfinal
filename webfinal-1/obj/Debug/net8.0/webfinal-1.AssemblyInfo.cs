bf5fb6fcf6c3625d89cf421a8c6c14a71d1dc5a2
kentwu9900 <kent0909hahaha@gamil.com>
2025-06-08 19:02:48 +0800

    新增排行榜功能

--- webfinal-1/obj/Debug/net8.0/webfinal-1.AssemblyInfo.cs
+++ webfinal-1/obj/Debug/net8.0/webfinal-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webfinal-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab1bec3343381747984152d1b6efe7f68c358cb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090a3ab9f6dbc46ccd34222ae13856d147cb00b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("webfinal-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webfinal-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
